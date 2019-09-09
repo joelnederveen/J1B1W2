9cc4b7b62988b98bc90cdce1cbe6194697f58a8a
Joel <99057548@mydavinci.nl>
2019-09-09 14:18:52 +0200

    lab 1

---
+++ b1w2l1.js
@@ -0,0 +1,17 @@
+// begin hier je JavaScript commandos
+
+
+window.alert("De gebruiker moet 3 vragen beantwoorden."); //Pop up
+
+var voor = prompt("Vul je voornaam in",); //vraag wat de voornaam is
+
+var achter = prompt("Vul je achternaam in",); //vraag wat de achternaam is
+
+var age = prompt("Vul je leeftijd in",);// vraag wat de leeftijd is
+
+document.write(voor); //antwoord vraag wat de voornaam is
+document.write ("<br/>") //volgende zin verder gaan voor netheid 
+document.write(achter);// antwoord vraag wat de achternaam is
+document.write ("<br/>")//volgende zin verder gaan voor netheid
+document.write(age); //antwoord op de vraag wat de leeftijd is
+
